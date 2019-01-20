f32d5c15663caa847b791be5db5cbbfb2c653c54
cosmic <leamonpaul@yandex.ru>
2019-01-20 07:06:52 +0300

    rockfm~3 rockfm.sh: Correctly move stderr to stdout

--- rockfm.sh
+++ rockfm.sh
@@ -7,7 +7,7 @@ elif test "$1" = "-o"
 then
     sudo touch /var/log/rockfm.log
     sudo chown $USER /var/log/rockfm.log
-    mpg123 http://nashe.streamr.ru/rock-64.mp3 2&>1 | cat >> /var/log/rockfm.log &
+    mpg123 http://nashe.streamr.ru/rock-64.mp3 2>&1 | cat >> /var/log/rockfm.log &
 else
     mpg123 http://nashe.streamr.ru/rock-64.mp3
 fi
