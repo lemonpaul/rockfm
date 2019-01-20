5abaa2441bc999afe76b2faf26513ee78fd6d810
cosmic <leamonpaul@yandex.ru>
2019-01-20 06:58:16 +0300

    rockfm~1 rockfm.sh: Added

---
+++ rockfm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if test "$1" = "-c"
+then
+    pkill mpg123
+elif test "$1" = "-o"
+then
+    sudo touch /var/log/rockfm.log
+    sudo chown $USER /var/log/rockfm.log
+    mpg123 http://nashe.streamr.ru/rock-64.mp3 2&>1 | cat >> /var/log/rockfm.log &
+else
+    mpg123 http://nashe.streamr.ru/rock-64.mp3
+fi
+
+exit 0
